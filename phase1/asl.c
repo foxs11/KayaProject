143daf423dcd90388d69b81c7b5f608838da9808
Samuel Fox <foxs11@xavier.edu>
2019-08-28 17:05:51 -0400

    broke something but am fixing it

--- phase1/asl.c
+++ phase1/asl.c
@@ -14,11 +14,11 @@
 static pcb_PTR semd_h;
 static pcb_PTR semdFree_h;
 
-// extern int insertBlocked (int *semAdd, pcb_PTR p){sammy}
-// extern pcb_PTR removeBlocked (int *semAdd){chris}
-// extern pcb_PTR outBlocked (pcb_PTR p){sammy}
-// extern pcb_PTR headBlocked (int *semAdd){chris}
-// extern void initASL (){chris}
+extern int insertBlocked (int *semAdd, pcb_PTR p){sammy}
+extern pcb_PTR removeBlocked (int *semAdd){chris}
+extern pcb_PTR outBlocked (pcb_PTR p){sammy}
+extern pcb_PTR headBlocked (int *semAdd){chris}
+extern void initASL (){chris}
 
 /***************************************************************/
 
