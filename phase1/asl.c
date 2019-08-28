c2717a57ec78db6cd9705368b24fc009176ceed9
foxs11 <samuel@cloverleaf.me>
2019-08-28 16:57:39 -0400

    testing

--- phase1/asl.c
+++ phase1/asl.c
@@ -1,25 +1,25 @@
-#ifndef ASL
-#define ASL
+// #ifndef ASL
+// #define ASL
 
-/************************** ASL.E ******************************
-*
-*  The externals declaration file for the Active Semaphore List
-*    Module.
-*
-*  Written by Mikeyg
-*/
+// /************************** ASL.E ******************************
+// *
+// *  The externals declaration file for the Active Semaphore List
+// *    Module.
+// *
+// *  Written by Mikeyg
+// */
 
-#include "../h/types.h"
+// #include "../h/types.h"
 
-static pcb_PTR semd_h;
-static pcb_PTR semdFree_h;
+// static pcb_PTR semd_h;
+// static pcb_PTR semdFree_h;
 
-extern int insertBlocked (int *semAdd, pcb_PTR p){sammy}
-extern pcb_PTR removeBlocked (int *semAdd){chris}
-extern pcb_PTR outBlocked (pcb_PTR p){sammy}
-extern pcb_PTR headBlocked (int *semAdd){chris}
-extern void initASL (){chris}
+// // extern int insertBlocked (int *semAdd, pcb_PTR p){sammy}
+// // extern pcb_PTR removeBlocked (int *semAdd){chris}
+// // extern pcb_PTR outBlocked (pcb_PTR p){sammy}
+// // extern pcb_PTR headBlocked (int *semAdd){chris}
+// // extern void initASL (){chris}
 
-/***************************************************************/
+// /***************************************************************/
 
-#endif
+// #endif
