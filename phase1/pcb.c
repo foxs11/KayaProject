1dff06bd82c6c61d040eec192a6c492d4d752445
foxs11 <samuel@cloverleaf.me>
2019-09-04 18:40:45 -0400

    asdf

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -49,9 +49,7 @@ void insertProcQ (pcb_PTR *tp, pcb_PTR p){
 
 void freePcb (pcb_PTR p){
   cleanPcb(p);
-  addokbuf("freePcb1\n");
   insertProcQ(&pcbFree_h, p);
-  addokbuf("freePbc2\n");
 }
 
 pcb_PTR outProcQ (pcb_PTR *tp, pcb_PTR p){
