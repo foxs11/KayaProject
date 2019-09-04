2a08fab7a7650fbbcd62094248251d19cdde8dae
foxs11 <samuel@cloverleaf.me>
2019-09-04 18:30:55 -0400

    adf

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -58,9 +58,14 @@ pcb_PTR outProcQ (pcb_PTR *tp, pcb_PTR p){
     return NULL;
   }
   else if(p==*tp){
-    (*tp)->p_next->p_prev = (*tp)->p_prev;
-    (*tp)->p_prev->p_next = (*tp)->p_next;
-    *tp = (*tp)->p_prev;
+    if((*tp->p_next == *tp)){
+      *tp = NULL;
+    }
+    else{
+      (*tp)->p_next->p_prev = (*tp)->p_prev;
+      (*tp)->p_prev->p_next = (*tp)->p_next;
+      *tp = (*tp)->p_prev;
+    }
     return (tailPointer);
   }
   else{
