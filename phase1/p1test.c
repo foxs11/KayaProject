ad1cc8641a1cf9f7c2b60a448be107091d245240
foxs11 <samuel@cloverleaf.me>
2019-09-05 18:15:41 -0400

    asdf

--- phase1/p1test.c
+++ phase1/p1test.c
@@ -275,7 +275,7 @@ void main() {
 	search2 = searchSemd(&sem[11]);
 	searched2 = search2->s_next->s_semAdd;
 	adebug(searched2, 0);
-	p = removeBlocked(&sem[12]);
+	
 	int *searched10 = NULL;
 	semd_PTR search10 = NULL;
 	search10 = searchSemd(&sem[12]);
