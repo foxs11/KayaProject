56daffe16b41475678a47ceabae93dacaa617339
cooperc5 <cooperchris309@gmail.com>
2019-09-04 23:16:09 -0400

    hopefully fixed asl

--- phase1/asl.c
+++ phase1/asl.c
@@ -25,28 +25,10 @@ HIDDEN void cleanSemd(semd_PTR p){
 
 HIDDEN int searchSemd(int *semd){
   semd_PTR semdListPTR = semd_h;
-  while(semdListPTR != NULL){
-    if(*semd == semdListPTR){
-      return TRUE;
-    }
-    else{
-      semdListPTR = semdListPTR->s_next; 
-    }
+  while (semdListPTR->s_next->s_semAdd < semd) {
+    semdListPTR = semdListPTR->s_next;
   }
-  return FALSE;
-}
-
-HIDDEN semd_PTR getsemdPTR(int *semd){
-  semd_PTR semdListPTR = semd_h;
-  while(semdListPTR != NULL){
-    if(semd == semdListPTR->s_semAdd){
-      return semdListPTR;
-    }
-    else{
-      semdListPTR = semdListPTR->s_next; 
-    }
-  }
-  return NULL;
+  return semdListPTR;
 }
 
 HIDDEN void freeSemd(semd_PTR p){
@@ -55,82 +37,94 @@ HIDDEN void freeSemd(semd_PTR p){
   semdFree_h = p;
 }
 
-HIDDEN void allocateSemd(int semAdd){
-  if(semdFree_h != NULL){
-    semd_PTR newSemd = semdFree_h;
-    if(semd_h == NULL){
-      semdFree_h = newSemd->s_next;
-      cleanSemd(newSemd);
-      semd_h = newSemd;
-      newSemd->s_semAdd = semAdd;
+HIDDEN semd_PTR allocateSemd(int semAdd){
+  if (semdFree_h == NULL) {
+    return NULL;
+  }
+  else {
+    /* one on free list */
+    if (semdFree_h->s_next == NULL) {
+      semd_PTR allocatedSemd = semdFree_h;
+      semdFree_h = NULL;
+      allocatedSemd->s_semAdd = semAdd;
+      return allocatedSemd;
     }
-    else{
-      semd_PTR currentInSemd = semd_h;
-      semd_PTR nextInSemd = semd_h->s_next;
-      while(semAdd > nextInSemd->s_semAdd){
-        currentInSemd = currentInSemd->s_next;
-        nextInSemd = nextInSemd->s_next;
-      }
-      semdFree_h = newSemd->s_next;
-      cleanSemd(newSemd);
-      newSemd->s_semAdd = semAdd;
-      currentInSemd->s_next = newSemd;
-      newSemd->s_next = nextInSemd;
+    else { /* more than one on free list */
+      semd_PTR allocatedSemd = semdFree_h;
+      semdFree_h = semdFree_h->s_next;
+      allocatedSemd->s_semAdd = semAdd;
+      return allocatedSemd;
     }
   }
 }
 
 extern int insertBlocked (int *semAdd, pcb_PTR p){
-  if(searchSemd(semAdd)){
-    insertProcQ(getsemdPTR(semAdd)->s_procQ, p);
-    p->p_semAdd = *semAdd;
+  semd_PTR parent = searchSemd(semAdd);
+  if(parent->s_next->s_semAdd == semAdd){
+    insertProcQ(parent->s_next->s_procQ, p);
+    p->p_semAdd = semAdd;
+    return FALSE;
   }
-  else{
-    allocateSemd(semAdd);
-    insertProcQ(getsemdPTR(semAdd)->s_procQ, p);
-    p->p_semAdd = *semAdd;
+  else{ /* semd not found and needs to be allocated */
+    semd_PTR newSemd = allocateSemd(semAdd);
+    if (newSemd == NULL) {
+      return TRUE; /* no more free semd's and insert is blocked */
+    }
+    newSemd->s_next = parent->s_next;
+    parent->s_next = newSemd;
+    insertProcQ(newSemd->s_procQ, p);
+    p->p_semAdd = semAdd;
+    return FALSE;
   }
 } /* search active semdList if found: insertProcQ(p and tp found in semd) if not found: allocate new semd, put new node into active list, perform found */
 
 extern pcb_PTR outBlocked (pcb_PTR p){
-  pcb_PTR toReturn = outProcQ(&getsemdPTR(p->p_semAdd)->s_procQ, p);
-  if(getsemdPTR(p->p_semAdd)->s_procQ == NULL){
-    freeSemd(getsemdPTR(p->p_semAdd));
-    return toReturn;
+  semd_PTR parent = searchSemd(p->p_semAdd);
+  if (parent->s_next->s_semAdd != p->p_semAdd) {
+    return NULL;
   }
-  else{
-    return toReturn;
+  else { /* p should be on the right process queue */
+    if (headProcQ(parent->s_next->s_procQ) == p) { /* are we removing the last pcb? */
+      pcb_PTR outedPcb = outProcQ(parent->s_next->s_procQ, p);
+      parent->s_next = parent->s_next->s_next;
+      freeSemd(parent->s_next);
+      return outedPcb;
+    }
+    return outProcQ(parent->s_next->s_procQ, p);
   }
 } /* search active semdList if not found: error case, if found: outProcQ on the process queue, value is returned. If processqueue not empty: your done, if it is empty: deallocate this semd node */
 
 extern pcb_PTR removeBlocked (int *semAdd){
-  if(!searchSemd(semAdd)){
+  semd_PTR parent = searchSemd(p->p_semAdd);
+  if (parent->s_next->s_semAdd != p->p_semAdd) {
     return NULL;
   }
-  else{
-    return outBlocked(getsemdPTR(semAdd)->s_procQ->p_next);
+  else { /* p should be on the right process queue */
+    if (headProcQ(parent->s_next->s_procQ) == p) { /* are we removing the last pcb? */
+      pcb_PTR outedPcb = removeProcQ(parent->s_next->s_procQ, p);
+      parent->s_next = parent->s_next->s_next;
+      freeSemd(parent->s_next);
+      return outedPcb;
+    }
+    return removeProcQ(parent->s_next->s_procQ, p);
   }
 }
 
 
 extern pcb_PTR headBlocked (int *semAdd){
-  if(!searchSemd(semAdd)){
+  semd_PTR parent = searchSemd(p->p_semAdd);
+  if (parent->s_next->s_semAdd != p->p_semAdd) {
     return NULL;
   }
-  else{
-    if(getsemdPTR(semAdd)->s_procQ == NULL){
-      return NULL;
-    }
-    else{
-      return getsemdPTR(semAdd)->s_procQ->p_next;
-    }
+  else { /* p should be on the right process queue */
+    return headProcQ(parent->s_next->s_procQ, p);
   }
 }
 
 extern void initASL (){
-  static semd_t semdTable[MAXPROC];
+  static semd_t semdTable[MAXPROC+2];
   int i;
-  for(i=0; i<MAXPROC; i++){
+  for(i=0; i<MAXPROC+2; i++){
     freeSemd(&semdTable[i]);
   }
 }
