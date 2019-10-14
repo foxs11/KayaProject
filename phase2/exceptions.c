b7e0d11b563ec228520077cac76a742fd6f8e963
foxs11 <samuel@cloverleaf.me>
2019-10-14 15:03:54 -0400

    asf

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -1,7 +1,8 @@
-#include "../phase1/pcb.c"
-#include "../phase1/asl.c"
+#include "../e/pcb.e"
+#include "../e/asl.e"
 #include "../h/types.h"
 #include "../h/const.h"
+#include "../e/initial.e"
 
 void pgmTrapHandler(){
   if (currentProcess->p_oldPgm == NULL){
