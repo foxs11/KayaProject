7c581a56edd73805814603cd72c0f5c376080d2e
foxs11 <samuel@cloverleaf.me>
2019-10-16 18:00:34 -0400

    asdf

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -289,11 +289,11 @@ void waitForIODevice(){
   addokbuf("in waitForIoDevice 1.4\n");
   int semNumber = getSemArrayNum(lineNumber, deviceNumber);
   addokbuf("in waitForIoDevice 1.5\n");
-  int *semAdd = &(devSemTable[semNumber]);
+  int *semAdd = devSemTable[semNumber];
   addokbuf("in waitForIoDevice 1.6\n");
   *semAdd--;
   addokbuf("in waitForIoDevice 1.7\n");
-  if ((*semAdd) < 0) {
+  if (*semAdd < 0) {
     addokbuf("in waitForIoDevice 2\n");
     cpu_t currTime = 0;
     addokbuf("in waitForIoDevice 3\n");
