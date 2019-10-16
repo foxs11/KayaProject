9cfbbdaa0b38cafb825ab17a1c6bc45602562993
foxs11 <samuel@cloverleaf.me>
2019-10-16 17:11:36 -0400

    asdf

--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -103,7 +103,7 @@ int getDevRegIndex(int lineNumber, int deviceNumber) {
 
 void interruptHandler(){
   cpu_t currTime = 0;
-  STCK(&currTime);
+  STCK(currTime);
   currentProcess->p_time = currentProcess->p_time + (currTime - (*time));
 
   state_t *interruptOld = (state_t *) INTERRUPTOLDAREA;
