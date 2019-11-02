f5a8fa6220f01953044fedd8f079e4170c4a8491
foxs11 <samuel@cloverleaf.me>
2019-11-02 14:15:44 -0400

    asdf

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -15,21 +15,21 @@ void bDebug(unsigned int a, unsigned int b) {
 }
 
 void scheduler(){
-  addokbuf("in scheduler 1")
+  addokbuf("in scheduler 1");
   unsigned int cp0status;
   pcb_PTR process = removeProcQ(&readyQue);
   if(process == NULL){
-    addokbuf("in scheduler 2")
+    addokbuf("in scheduler 2");
     if(processCount == 0){
       HALT();
     }
     else{
-      addokbuf("in scheduler 3")
+      addokbuf("in scheduler 3");
       if(softBlockCount == 0){
         PANIC();
       }
       else{
-        addokbuf("in scheduler 4")
+        addokbuf("in scheduler 4");
         cp0status = SCHEDULERINTSUNMASKED;
         waitFlag = 1;
         setSTATUS(cp0status); /* has a return value...?  */
@@ -38,13 +38,13 @@ void scheduler(){
     }
   }
   else{
-    addokbuf("in scheduler 5")
+    addokbuf("in scheduler 5");
     waitFlag = 0;
     currentProcess = process;
     setTIMER(5000);
-    addokbuf("in scheduler 6")
+    addokbuf("in scheduler 6");
     STCK(time);
-    addokbuf("in scheduler 7")
+    addokbuf("in scheduler 7");
     LDST(&(process->p_s));
   }
 }
