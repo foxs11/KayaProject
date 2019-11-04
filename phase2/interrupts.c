bad0736e990f9cbc828162f7346525a6fa073830
foxs11 <samuel@cloverleaf.me>
2019-11-04 00:53:54 -0500

    i hate everything

--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -127,6 +127,7 @@ int getDevRegIndex(int lineNumber, int deviceNumber) {
 }
 
 void interruptHandler(){
+	addokbuf("in interupt handler\n");
 	unsigned int status = 0;
 	int termOffset = 0;
   cpu_t currTime = 0;
