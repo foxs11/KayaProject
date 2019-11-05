b56a0876bb6ce9b4a36b6f51360b6a4b013bd8b7
foxs11 <samuel@cloverleaf.me>
2019-11-05 00:48:46 -0500

    i hate everything

--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -191,7 +191,7 @@ void interruptHandler(){
 				if (devSemTable[(EIGHTDEVLINES * DEVSPERLINE) + DEVSPERLINE] <= 0){
 					while (headBlocked(&(devSemTable[(EIGHTDEVLINES * DEVSPERLINE) + DEVSPERLINE])) != NULL) {
 						pcb_PTR temp = removeBlocked(&(devSemTable[EIGHTDEVLINES * DEVSPERLINE + DEVSPERLINE]));
-						(*(devSemTable[(EIGHTDEVLINES * DEVSPERLINE) + DEVSPERLINE]))++;
+						(*devSemTable[(EIGHTDEVLINES * DEVSPERLINE) + DEVSPERLINE])++;
             insertProcQ(&readyQue, temp);
             softBlockCount--;
           }
